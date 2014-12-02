245326770057247241342fc0ff02118f586a9f87
danhamilt1 <daniel2.hamilton@live.uwe.ac.uk>
2014-12-02 10:51:00 +0000

    changed tests

--- ws1/testRun.sh
+++ ws1/testRun.sh
@@ -7,11 +7,11 @@ printf "TEST_NUM, MATCHES, GENERATION\n" >> out.txt
 make clean
 make
 
-rm ~/Desktop/results/*.csv
+
 
 _now=$(date +"%m_%d_%Y_%s")
 
-while [ $i -lt 50 ]
+while [ $i -lt 5 ]
 do
 
   printf "$i, " >> out.txt
@@ -21,14 +21,16 @@ do
 
   _history_now=${_now}_plots_${i}
   _file2="$_history_now.csv"
-  cp ./history.csv ~/Desktop/results/$_file2
+  cp ./history.csv ./results/$_file2
 done
 
 
 _file="$_now.csv"
 
-cp ./out.txt ~/Desktop/results/$_file
+cp ./out.txt ./results/$_file
 rm out.txt
 touch out.txt
 
-{ head -n1 ~/Desktop/results/*plots_1.csv; for f in ~/Desktop/results/*plots*.csv; do printf "\n"; tail -n+2 "$f"; done; } > ~/Desktop/results/op.csv
+{ head -n1 ./results/*plots_1.csv; for f in ./results/*plots*.csv; do printf "\n"; tail -n+2 "$f"; done; } > ./results/merged_$_now.csv
+
+rm ./results/*plots*.csv
